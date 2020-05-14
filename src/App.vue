221dadca9e5bf73f894ab41b464e60fd3c727ebf
kurob1993 <kurob1993@gmail.com>
2020-05-14 21:20:47 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header />
+    <LeftSidebar />
+    <div class="page-wrapper">
+      <div class="container-fluid">
+        <StarterKit />
+        <RightSidebar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header'
+import LeftSidebar from './components/layout/LeftSidebar'
+import RightSidebar from './components/layout/RightSidebar'
+import StarterKit from './components/StarterKit'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    LeftSidebar,
+    RightSidebar,
+    StarterKit
+  }
+}
+</script>
---
+++ src/components/layout/LeftSidebar.vue
@@ -0,0 +1,80 @@
+<template>
+  <aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+      <!-- Sidebar navigation-->
+      <nav class="sidebar-nav">
+        <ul id="sidebarnav">
+          <li class="user-pro">
+            <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false" >
+              <img src="images/users/1.jpg" alt="user-img" class="img-circle" />
+              <span class="hide-menu">Mark Jeckson</span>
+            </a>
+            <ul aria-expanded="false" class="collapse">
+              <li>
+                <a href="javascript:void(0)">
+                  <i class="ti-user"></i> My Profile
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0)">
+                  <i class="ti-wallet"></i> My Balance
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0)">
+                  <i class="ti-email"></i> Inbox
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0)">
+                  <i class="ti-settings"></i> Account Setting
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0)">
+                  <i class="fa fa-power-off"></i> Logout
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-small-cap">--- PERSONAL</li>
+          <li>
+            <a class="waves-effect waves-dark" href="index.html">
+              <i class="icon-speedometer"></i>
+              <span class="hide-menu">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false" >
+              <i class="ti-layout-grid2"></i>
+              <span class="hide-menu">Apps</span>
+            </a>
+            <ul aria-expanded="false" class="collapse">
+              <li>
+                <a href="app-calendar.html">Calendar</a>
+              </li>
+              <li>
+                <a href="app-chat.html">Chat app</a>
+              </li>
+              <li>
+                <a href="app-ticket.html">Support Ticket</a>
+              </li>
+              <li>
+                <a href="app-contact.html">Contact / Employee</a>
+              </li>
+              <li>
+                <a href="app-contact2.html">Contact Grid</a>
+              </li>
+              <li>
+                <a href="app-contact-detail.html">Contact Detail</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+  </aside>
+</template>
